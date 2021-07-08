1fa33877fed4f77333eb22533c7f1d64750c1c50
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-08 18:07:47 -0400

    everything works with forEach in fetch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
    fetchAllDogs()
-   renderDogList()
-   renderPupCard()
 })
 
 function fetchAllDogs(dog) {
     fetch(`http://localhost:3000/pups`)
     .then(response => response.json())
-    .then(json => {             //console.log(json.forEach(dog)))
+    .then(json => {
         json.forEach((dog => {
             renderDogList(json)
         }))
@@ -24,29 +22,19 @@ function renderDogList(dog) {
         dogNameDiv.append(spanName)
     
         spanName.addEventListener('click', (e) => {
-            e.preventDefault
             pupCardDiv.innerHTML = ""
             renderPupCard(dog)          
         })
     })
 }
+
 function renderPupCard(dog) {
     let pupCardDiv = document.querySelector("#dog-info");
-        dog.forEach((dog) => {
-            let h2Name = document.createElement('h2');
-            let imgPup = document.createElement('img')
-            let buttonPup = document.createElement('button')
-            h2Name.textContent = dog.name
-            imgPup.src = dog.image
-            buttonPup.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"    
-            pupCardDiv.append(h2Name, imgPup, buttonPup)  
-    })   
+    let h2Name = document.createElement('h2');
+    let imgPup = document.createElement('img')
+    let buttonPup = document.createElement('button')
+    h2Name.textContent = dog.name
+    imgPup.src = dog.image
+    buttonPup.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"    
+    pupCardDiv.append(h2Name, imgPup, buttonPup)  
 }
-// pupCardDiv.interHTML ="" 
-            // let h2Name = document.createElement('h2');
-            // let imgPup = document.createElement('img')
-            // let buttonPup = document.createElement('button')
-            // h2Name.textContent = dog.name
-            // imgPup.src = dog.image
-            // buttonPup.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
-            // pupCardDiv.append(h2Name, imgPup, buttonPup) 
